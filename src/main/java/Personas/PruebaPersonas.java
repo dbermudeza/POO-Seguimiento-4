9854183fac1877950281c3c9f6f6743a6fc5107d
DavidZapataN <dzapatan@unal.edu.co>
2023-10-15 13:18:28 -0500

    Ejercicio 8.1 – Inicia en página 467

---
+++ src/main/java/Personas/PruebaPersonas.java
@@ -0,0 +1,9 @@
+package Personas;
+
+public class PruebaPersonas {
+
+    public static void main(String[] args) {      
+    VentanaPrincipal miVentanaPrincipal; 
+    miVentanaPrincipal= new VentanaPrincipal();     miVentanaPrincipal.setVisible(true);
+    }
+}
